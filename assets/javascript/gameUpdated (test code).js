6499f034771f07117f304229bead1cb56d6fe53c
admin <admin>
2017-08-07 23:43:17 -0400

    final commit with unclean code

--- assets/javascript/gameUpdated (test code).js
+++ assets/javascript/gameUpdated (test code).js
@@ -7,100 +7,81 @@
 //function when score to get <= current score, it is a loss
 
 var randomNumberToGet = Math.floor((Math.random() * 120) + 19);
-var randomCrystalValue1 = Math.floor((Math.random() * 12) + 1);
-var randomCrystalValue2 = Math.floor((Math.random() * 12) + 1);
-var randomCrystalValue3 = Math.floor((Math.random() * 12) + 1);
-var randomCrystalValue4 = Math.floor((Math.random() * 12) + 1);
 
 var scoreToGet = 0;
-var currentScore = 0;
+var currentScore;
 var wins = 0;
 var losses = 0;
-var crystalValue1 = 0;
-var crystalValue2 = 0;
-var crystalValue3 = 0;
-var crystalValue4 = 0;
-
-var gameStart = true;
-
-	if (gameStart === true) {
-		
-		scoreToGet = randomNumberToGet;
-		crystalValue1 = randomCrystalValue1;
-		crystalValue2 = randomCrystalValue2;
-		crystalValue3 = randomCrystalValue3;
-		crystalValue4 = randomCrystalValue4;
-
-		$(".scoreToGet").html(scoreToGet);
-		$(".currentScore").html(currentScore);
-		$(".wins").html(wins);
-		$(".losses").html(losses);
+var crystalValue1;
+var crystalValue2;
+var crystalValue3;
+var crystalValue4;
 
 		var checkScore = function() {
 			if (currentScore === scoreToGet) {
 				wins++;
 				$(".wins").html(wins);
-				gameStart = false;
 			}
 
 			else if (currentScore > scoreToGet) {
 				losses++;
 				$('.losses').html(losses);
-				gameStart = false;	
 			}
 		};
+
 		var reset = function() {
-			if (gameStart === false){
 				
 				currentScore = 0;
 				randomNumberToGet = Math.floor((Math.random() * 120) + 19);
-				var randomCrystalValue1 = Math.floor((Math.random() * 12) + 1);
-				var randomCrystalValue2 = Math.floor((Math.random() * 12) + 1);
-				var randomCrystalValue3 = Math.floor((Math.random() * 12) + 1);
-				var randomCrystalValue4 = Math.floor((Math.random() * 12) + 1);
+				var crystalValue1 = Math.floor((Math.random() * 12) + 1);
+				var crystalValue2 = Math.floor((Math.random() * 12) + 1);
+				var crystalValue3 = Math.floor((Math.random() * 12) + 1);
+				var crystalValue4 = Math.floor((Math.random() * 12) + 1);
 
 				scoreToGet = randomNumberToGet;
-				crystalValue1 = randomCrystalValue1;
-				crystalValue2 = randomCrystalValue2;
-				crystalValue3 = randomCrystalValue3;
-				crystalValue4 = randomCrystalValue4;
 				
 				$(".currentScore").html(currentScore);
 				$(".scoreToGet").html(scoreToGet);
-				gameStart = true;
-			}
+
+				console.log(crystalValue1);
+				console.log(crystalValue2);
+				console.log(crystalValue3);
+				console.log(crystalValue4);
 		};
+	
+	reset();
+		
+		scoreToGet = randomNumberToGet;
+
+		$(".scoreToGet").html(scoreToGet);
+		$(".currentScore").html(currentScore);
+		$(".wins").html(wins);
+		$(".losses").html(losses);
+		console.log("It resets");
+		
 		
 		$(".crystal-1").on("click", function() {
 			currentScore = currentScore + crystalValue1;
 			$(".currentScore").html(currentScore);
+			console.log("It clicks");
+			console.log(currentScore);
 			checkScore();
-			if (gameStart === false){
-				reset();
-			}
 		});
 
 		$(".crystal-2").on("click", function() {
 			currentScore = currentScore + crystalValue2;
 			$(".currentScore").html(currentScore);
 			checkScore();
-			reset();
 		});
 
 		$(".crystal-3").on("click", function() {
 			currentScore = currentScore + crystalValue3;
 			$(".currentScore").html(currentScore);
 			checkScore();
-			reset();
 		});
 
 		$(".crystal-4").on("click", function() {
 			currentScore = currentScore + crystalValue4;
 			$(".currentScore").html(currentScore);
 			checkScore();
-			reset();
 		});
-
-
-		
-	}
